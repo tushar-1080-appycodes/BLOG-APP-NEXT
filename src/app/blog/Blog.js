6669ad969c4fa4d08c2b2fbed72975142bc22b99
tushar-1080-appycodes <tushar@appycodes.com>
2025-03-29 13:11:40 +0530

    zzz

--- src/app/blog/Blog.js
+++ src/app/blog/Blog.js
@@ -1,22 +1,17 @@
 "use client";
 
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { getDocs, collection } from "firebase/firestore";
 import { db } from "@/firebase/config";
-import { increase } from "@/features/blog/blogSlice";
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import BlogCard from "./BlogCard";
 
 export default function Blogs() {
-  const blog = {
-    image: "/next.svg",
-    title: "Blog Title",
-    desc: "Blog Description",
-  };
-
   const router = useRouter();
+
   const blogCount = useSelector((state) => state.blog.addedBlog);
+
   const [blogs, setBlogs] = useState([]);
 
   const fetchBlogs = async () => {
@@ -39,33 +34,8 @@ export default function Blogs() {
 
   return (
     <div className="blogsContainer">
-      {[...Array(12)].map((_, index) => (
-        <div key={index} className="blogCard">
-          <Image src="/next.svg" width={500} height={500} alt="zzz"></Image>
-          <h3>Blog Title</h3>
-          <p>Blog Description</p>
-          <button
-            onClick={() =>
-              router.push(
-                `/blog/${index}?image=${blog.image}&title=${blog.title}&desc=${blog.desc}`
-              )
-            }
-          >
-            Read More
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
-        </div>
+      {blogs.map((blog, index) => (
+        <BlogCard key={index} title={blog.title} desc={blog.desc} imgURL={blog.imgURL} />
       ))}
     </div>
   );
---
+++ src/app/blog/BlogCard.jsx
@@ -0,0 +1,35 @@
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
+export default function BlogCard({title,desc,imgURL="/next.svg",index}) {
+  const router = useRouter();
+
+  return (
+    <div key={index} className="blogCard">
+      <Image src="/next.svg" width={500} height={500} alt="zzz"></Image>
+      <h3>Blog Title</h3>
+      <p>Blog Description</p>
+      <button
+        onClick={() =>
+          router.push(
+            `/blog/${index}?image=${imgURL}&title=${title}&desc=${desc}`
+          )
+        }
+      >
+        Read More
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            fillRule="evenodd"
+            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+}
