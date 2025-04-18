0654928190757cda2a931674090a0a8028bc264b
Tushar <tushar@appycodes.com>
2025-04-18 12:32:36 +0530

    changed some styling on log in

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -16,9 +16,9 @@ export default function AuthLayout({ children }) {
   const pathname = usePathname();
 
   return (
-    <Card className="p-2 bg-black text-white self-center flex flex-col gap-4 border-2 rounded-2xl mt-[20%] lg:my-[2%] w-[90%] md:w-[80%] lg:w-1/3">
+    <Card className="p-4 bg-black text-white  flex flex-col justify-center items-start jus gap-4 border-2 rounded-2xl mx-5 mt-[20%] lg:mt-[15%] w-fit">
       <CardHeader>
-        <CardTitle className="text-5xl md:text-7xl p-4">{pathname === "/logIn" ? "Log In" : "Sign Up"}</CardTitle>
+        <CardTitle className="text-5xl md:text-7xl text-nowrap p-4">{pathname === "/logIn" ? "Log In" : "Sign Up"}</CardTitle>
         <CardDescription></CardDescription>
       </CardHeader>
       <CardContent>
@@ -30,7 +30,7 @@ export default function AuthLayout({ children }) {
             don&apos;t have an account? <Link className="border-b-2 border-b-white" href="/signUp">Sign Up</Link>
           </p>
         ) : (
-          <p className="mx-auto my-1.5">
+          <p className="mx-auto my-1.5 text-center">
             already have an account? <Link className="border-b-2 border-b-white" href="/logIn">Log In</Link>
           </p>
         )}
